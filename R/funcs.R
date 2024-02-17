44be7ec7aa47ca6a5233d4ba3c966644ed868231
fawda123 <mbafs2012@gmail.com>
2024-02-17 12:18:02 -0500

    add option for stacked barplot by subembayment

--- R/funcs.R
+++ R/funcs.R
@@ -1,20 +1,44 @@
 # plotting function
 # raw data or summarized
-plo_fun <- function(seldat, sumsel, yearsel){
+plo_fun <- function(seldat, sumsel, yearsel, barsel){
 
   req(seldat)
+
+  if(!barsel){
+
+    seldat <- summarise(seldat,
+                     mag = sum(mag, na.rm = T),
+                     .by = c('month', 'year', 'date', 'param'))
+  }
 
   # raw data
   if(!sumsel){
 
-    # xaxis range
-    xrng <- range(seldat$date)
-    ymax <- max(seldat$mag, na.rm = T)
+    # x axis ranges
+    xrng <- range(seldat$date) + c(-15, 15)
 
-    # plot
-    p <- plotly::plot_ly(seldat, x = ~date, y = ~mag, type = 'scatter', mode = 'lines+markers',
-                         marker = list(color = 'rgb(0, 174, 239)'),
-                         line = list(color = 'rgb(0, 174, 239)'))
+    if(!barsel){
+
+      # yaxis range
+      ymax <- max(seldat$mag, na.rm = T)
+
+      # plot
+      p <- plotly::plot_ly(seldat, x = ~date, y = ~mag, type = 'scatter', mode = 'lines+markers',
+                           marker = list(color = 'rgb(0, 174, 239)'),
+                           line = list(color = 'rgb(0, 174, 239)'))
+
+    }
+
+    if(barsel){
+
+      # axis ranges
+      ymax <- NA
+
+      # plot
+      p <- plotly::plot_ly(seldat, x = ~date, y = ~mag, type = 'bar', color = ~POTW, colors = 'Set1')
+
+    }
+
   }
 
   # summarized data
@@ -35,24 +59,47 @@ plo_fun <- function(seldat, sumsel, yearsel){
 
     # summarize data
     seldat <- seldat %>%
-      filter(month >= tofilt[1] & month <= tofilt[2]) %>%
-      summarise(
-        ave = mean(mag, na.rm = T),
-        lov = tryCatch(t.test(mag, na.rm = T)$conf.int[1], error = function(err) NA),
-        hiv = tryCatch(t.test(mag, na.rm = T)$conf.int[2], error = function(err) NA),
-        .by = year
-      ) %>%
-      mutate(
-        ave = ifelse(is.na(lov), NA, ave)
-      )
+      filter(month >= tofilt[1] & month <= tofilt[2])
 
-    # ymax
-    ymax <- max(seldat$hiv, na.rm = T)
+    if(!barsel){
 
-    # plot
-    p <- plotly::plot_ly(seldat, x = ~year, y = ~ave, type = 'scatter', mode = 'lines',
-                         line = list(color = 'rgb(0, 174, 239)')) %>%
-      plotly::add_ribbons(ymin = ~lov,  ymax = ~hiv, fillcolor = 'rgba(0, 174, 239, 0.2)', line = list(color = 'rgba(0, 174, 239, 0)'))
+      seldat <- seldat %>%
+        summarise(
+          ave = mean(mag, na.rm = T),
+          lov = tryCatch(t.test(mag, na.rm = T)$conf.int[1], error = function(err) NA),
+          hiv = tryCatch(t.test(mag, na.rm = T)$conf.int[2], error = function(err) NA),
+          .by = year
+        ) %>%
+        mutate(
+          ave = ifelse(is.na(lov), NA, ave)
+        )
+
+      # ymax
+      ymax <- max(seldat$hiv, na.rm = T)
+
+      # plot
+      p <- plotly::plot_ly(seldat, x = ~year, y = ~ave, type = 'scatter', mode = 'lines',
+                           line = list(color = 'rgb(0, 174, 239)')) %>%
+        plotly::add_ribbons(ymin = ~lov,  ymax = ~hiv, fillcolor = 'rgba(0, 174, 239, 0.2)', line = list(color = 'rgba(0, 174, 239, 0)'))
+
+    }
+
+    if(barsel){
+
+      seldat <- seldat %>%
+        summarise(
+          mag = sum(mag, na.rm = T),
+          .by = c('year', 'POTW')
+        )
+
+      # ymax
+      ymax <- NA
+      xrng <- xrng + c(-0.5, 0.5)
+
+      # plot
+      p <- plotly::plot_ly(seldat, x = ~year, y = ~mag, type = 'bar', color = ~POTW, colors = 'Set1')
+
+    }
 
   }
 
@@ -60,7 +107,8 @@ plo_fun <- function(seldat, sumsel, yearsel){
     plotly::layout(
       xaxis = list(title = NA, range = xrng),
       yaxis = list(title = 'Magnitude', range = c(0, ymax)),
-      showlegend = F
+      showlegend = F,
+      barmode = 'stack'
     )
 
   # print
